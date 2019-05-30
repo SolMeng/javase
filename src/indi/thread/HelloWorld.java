6ec08fe2601a47c4612a125a65b49ddfdf750289
SolMeng <740620871@qq.com>
2019-05-31 00:35:50 +0800

    idea上传本地项目到github

---
+++ src/indi/thread/HelloWorld.java
@@ -0,0 +1,13 @@
+package indi.thread;
+
+/**
+ * @program: javase
+ * @description: github上传测试
+ * @author: Sol Meng
+ * @create: 2019-05-31
+ **/
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("hello gitHub");
+    }
+}
